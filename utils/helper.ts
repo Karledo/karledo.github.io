f2471b6c1617b723608f2ee6109f7a2f3313e198
Karl Edochie <karledochie8@gmail.com>
2025-03-24 01:35:27 +0000

    Add dynamic title and update dates

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,3 +1,5 @@
+import { pages } from "@/app/writing/page-data";
+
 export function formatBasename(name: string) {
   return name
     .split("-")
@@ -5,4 +7,16 @@ export function formatBasename(name: string) {
       return word.charAt(0).toUpperCase() + word.slice(1);
     })
     .join(" ");
+}
+
+export function getPageData(slug: string) {
+  const pageData = pages.find((page) => {
+    return page.slug == slug;
+  });
+  if (pageData == undefined) {
+    throw Error(
+      "Attempting to get the informatio of a page which does not exist",
+    );
+  }
+  return pageData;
 }
