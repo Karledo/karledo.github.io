bc70fb210e4ba7aa0a3d185c5f6061a189a019b7
Karl Edochie <karledochie8@gmail.com>
2025-10-08 13:35:42 +0100

    Add collapse to wave function

--- app/writing/(content)/the-schrodinger-equation/components/wave-function.tsx
+++ app/writing/(content)/the-schrodinger-equation/components/wave-function.tsx
@@ -6,25 +6,43 @@ import katex from "katex";
 import P5 from "p5";
 
 const MAX_WIDTH = 641.52;
-const setup: Setup = ({ p, containerStyle, state }) => {
+const setup: Setup = ({ p, renderer, containerStyle, state }) => {
   p.colorMode(p.HSL);
   p.rectMode(p.CORNERS);
 
   const axisNames = ["psi", "x"];
   const axis = Object.fromEntries(
     axisNames.map((value) => {
-      return [value, p.createP().style("font-size", "1rem").style("color", containerStyle.color)];
+      return [
+        value,
+        p
+          .createP()
+          .style("font-size", `${p.width * 0.0025}rem`)
+          .style("color", containerStyle.color),
+      ];
     }),
   );
 
   state.axis = axis;
+
+  renderer.mousePressed((event: MouseEvent) => {
+    state.collapseX = event.clientX - (renderer.elt as HTMLCanvasElement).getBoundingClientRect().x;
+  });
+
+  renderer.mouseReleased(() => {
+    state.collapseX = undefined;
+  });
 };
 
-const draw: Draw = ({ p, state }) => {
+const gaussianFn = (x: number, a: number, b: number) => {
+  return Math.exp(-(Math.pow(x - a, 2) / b));
+};
+
+const draw: Draw = ({ p, renderer, state }) => {
   p.clear();
   p.noStroke();
 
-  const axis = state.axis as { [k: string]: P5.Element };
+  const axis = state.axis as Record<string, P5.Element>;
 
   const documentStyle = getComputedStyle(document.documentElement);
   const foreground = documentStyle.getPropertyValue("--foreground-100");
@@ -36,13 +54,22 @@ const draw: Draw = ({ p, state }) => {
   const timeScale = 0.3;
 
   axis.psi.position(p.width * 0.52, p.height * 0.01);
+  axis.psi.style("font-size", `${p.width * 0.0015}rem`);
   katex.render(String.raw`\psi(x, t)^2`, axis.psi.elt);
 
+  axis.x.style("font-size", `${p.width * 0.0015}rem`);
   axis.x.position(p.width * 0.97, p.height * 0.41);
   katex.render(String.raw`x`, axis.x.elt);
 
+  const collapseX = state.collapseX as Partial<number>;
+
+  const collapseFn = (x: number) => 100 * gaussianFn(x, collapseX, 10);
+  const normalWaveFunction = (x: number) => p.noise(x * noiseScale, seconds * timeScale) * noiseHeight;
+
+  const fn = collapseX ? collapseFn : normalWaveFunction;
+
   p.stroke(background);
-  p.strokeWeight((2 * p.width) / MAX_WIDTH);
+  p.strokeWeight(p.width * 0.004);
   p.line(0, p.height * 0.5, p.width, p.height * 0.5);
   p.line(p.width * 0.5, 0, p.width * 0.5, p.height);
 
@@ -50,15 +77,7 @@ const draw: Draw = ({ p, state }) => {
   p.strokeWeight((3 * p.width) / MAX_WIDTH);
   p.stroke(foreground);
   for (let x = 0; x < p.width; x++) {
-    const noiseX = x * noiseScale;
-    const nextNoiseX = (x + 1) * noiseScale;
-
-    p.line(
-      x,
-      -p.noise(noiseX, seconds * timeScale) * noiseHeight,
-      x + 1,
-      -p.noise(nextNoiseX, seconds * timeScale) * noiseHeight,
-    );
+    p.line(x, -fn(x), x + 1, -fn(x + 1));
   }
 };
 
