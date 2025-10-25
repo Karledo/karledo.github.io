d76f4f9190d8b8f7315e4766c16ef86ef40717d0
Karl Edochie <karledochie8@gmail.com>
2025-10-25 15:22:20 +0100

    Update metadataBase

--- app/writing/(content)/layout.tsx
+++ app/writing/(content)/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_SITE_URL}/writing)`),
+  metadataBase: new URL("https://karledo.github.io/writing"),
 };
 
 export default function Layout({
